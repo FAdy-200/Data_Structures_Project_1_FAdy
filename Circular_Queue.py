3cbeeac0009b562aebfd960fa87f9a409b7cb39f
FAdy200 <abuyehia200@gmail.com>
2020-12-22 23:57:06 +0200

    DONE

--- Circular_Queue.py
+++ Circular_Queue.py
@@ -6,21 +6,7 @@ class CircularDeque:
         """
         self.capacity = capacity
         self.deque = [None] * capacity
-        self.front = self.rear = -1
-
-    def insertFront(self, value: int) -> bool:
-        """
-        Adds an item at the front of Deque.
-        Return true if the operation is successful.
-        """
-        if self.isFull():
-            return False
-        self.front += 1
-        self.front = self.front % self.capacity
-        self.deque[self.front] = value
-        if self.front == 0 and self.rear < 0:
-            self.rear = 0
-        return True
+        self.rear = self.front = -1
 
     def insertRear(self, value: int) -> bool:
         """
@@ -29,24 +15,24 @@ class CircularDeque:
         """
         if self.isFull():
             return False
-        self.rear -= 1
-        if self.rear < 0: self.rear = self.capacity - 1
-        if self.front < 0: self.front = self.capacity - 1
+        self.rear += 1
+        self.rear = self.rear % self.capacity
         self.deque[self.rear] = value
+        if self.rear == 0 and self.front < 0:
+            self.front = 0
         return True
 
-    def deleteFront(self) -> bool:
+    def insertFront(self, value: int) -> bool:
         """
-        Deletes an item from the front of Deque.
+        Adds an item at the front of Deque.
         Return true if the operation is successful.
         """
-        if self.isEmpty():
+        if self.isFull():
             return False
         self.front -= 1
         if self.front < 0: self.front = self.capacity - 1
-        if self.front == self.rear - 1:
-            self.front = -1
-            self.rear = -1
+        if self.rear < 0: self.rear = self.capacity - 1
+        self.deque[self.front] = value
         return True
 
     def deleteRear(self) -> bool:
@@ -56,28 +42,42 @@ class CircularDeque:
         """
         if self.isEmpty():
             return False
-        self.rear += 1
-        self.rear = self.rear % self.capacity
-        if (self.front == self.rear + 1) or (self.rear == 0 and self.front == self.capacity - 1):
-            self.front = -1
+        self.rear -= 1
+        if self.rear < 0: self.rear = self.capacity - 1
+        if self.rear == self.front - 1:
             self.rear = -1
+            self.front = -1
         return True
 
-    def getFront(self):
+    def deleteFront(self) -> bool:
         """
-        Get the front item from the deque.
+        Deletes an item from the front of Deque.
+        Return true if the operation is successful.
+        """
+        if self.isEmpty():
+            return False
+        self.front += 1
+        self.front = self.front % self.capacity
+        if (self.rear == self.front + 1) or (self.front == 0 and self.rear == self.capacity - 1):
+            self.rear = -1
+            self.front = -1
+        return True
+
+    def getRear(self):
+        """
+        Get the rear item from the deque.
         """
         if self.isEmpty():
             return None
-        return self.deque[self.front]
+        return self.deque[self.rear]
 
-    def getRear(self):
+    def getFront(self):
         """
         Get the last item from the deque.
         """
         if self.isEmpty():
             return None
-        return self.deque[self.rear]
+        return self.deque[self.front]
 
     def isEmpty(self) -> bool:
         """
@@ -89,7 +89,7 @@ class CircularDeque:
         """
         Checks whether the circular deque is full or not.
         """
-        front = (self.front+1) % self.capacity
-        if front == self.rear:
+        rear = (self.rear + 1) % self.capacity  # a more delicate solution
+        if rear == self.front:
             return True
         return False
