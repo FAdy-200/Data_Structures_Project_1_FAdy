1147d55bc21b82d3cf60694314765ea6dcde9655
FAdy200 <abuyehia200@gmail.com>
2020-12-24 13:03:28 +0200

    NEED WORK

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from AVL_Tree import AVLTree
 from Circular_Queue import CircularDeque
 
-
 def printLevelOrder(root):
+    deq = CircularDeque()
     """
     Given root node, print the root tree level by level
     """
@@ -11,11 +11,12 @@ def printLevelOrder(root):
     ##                      YOUR CODE HERE                      ##
     ##############################################################
 
-# myTree = AVLTree()
-# root = None
-# nums = [33, 13, 52, 9, 21, 61, 8, 11]
-# for num in nums:
-#     root = myTree.insert(root, num)
-# myTree.printLevelOrder(root)
-# myTree.delete(root, 13)
-# myTree.printLevelOrder(root)
+
+myTree = AVLTree()
+root = None
+nums = [33, 13, 52, 9, 21, 61, 8, 11]
+for num in nums:
+    root = myTree.insert(root, num)
+printLevelOrder(root)
+myTree.delete(root, 13)
+printLevelOrder(root)
